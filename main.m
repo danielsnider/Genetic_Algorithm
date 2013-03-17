f66eda83d56ddcdb3f3880c4a16de3209495e74a
root <root@dan-cv.local>
2013-03-17 01:07:07 +0100

    tweaked probability operators

--- main.m
+++ main.m
@@ -1,53 +1,58 @@
-% queen Y-coord = list index number
-% queen X-coord = list value (stored in 3 binary bits)
-
-
-% init vars
-% chroms = list of 8x3 binary matricies
+runloops = 40000; % number of loops (estimated runtime for 20000 run loops: 10 minutes)
 num_queens = 8;
 bit_depth = 3;
-chroms = {round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth))}; % pop is 6
 pop = 6; % must be an even number because a child must have 2 parents
 cost = zeros(pop,1);
 fitness = zeros(pop,1);
 roulette = zeros(pop,1);
-crossover_prob = 0.3;
-mutation_prob = 0.01;
+crossover_prob = 0.003;
+mutation_prob = 0.0001;
 child1 = zeros(num_queens,bit_depth);
 child2 = zeros(num_queens,bit_depth);
 temp = zeros(num_queens,bit_depth);
+cost_history = [];
 
-for x=1:3000,
+chroms = {round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth)), round(rand(num_queens,bit_depth))}; % pop is 6
+
+% chroms = list of 8x3 binary matricies (each row is a queen represented with:
+% chroms list index number = Y-coord
+% chroms list value (stored in binary 3 bits) = X-coord
+
+for x=1:runloops,
 
     %%% calc fitness of each chrom (lower is better)
     for i=1:pop,
         cost(i)=fitnessFunc(chroms{i});
         if cost(i)==0,
-            binToDec(chroms{i})
+            binToDec(chroms{i}) % print a solution to puzzle
+            return
         end
     end
-    fitness = abs(cost.-max(cost)) + 1; % convert cost to fitness (ie. conversion from lower is better to higher is better)
+
+    % save all fitness values for graphing later
+    cost_history = [cost_history; cost];
 
     %%% calc roulette wheel
+    fitness = abs(cost.-max(cost)) + 1; % convert cost to fitness (ie. conversion from lower is better to higher is better)
     roulette_wheel = cumsum(fitness.*(100/sum(fitness)));
 
     %%% operations to produce next generation (parent selection, crossover, mutation)
     for i=1:pop/2,
 
         %%% perform parent selection
-        r1 = rand*100;
-        r2 = rand*100;
         found_parent1 = false;
         found_parent2 = false;
+        r1 = rand*100;
+        r2 = rand*100;
 
         for j=1:pop,
             if (r1 < roulette_wheel(j)) && (found_parent1 == false),
                 found_parent1 = true;
-                child1 = chroms{j}; % child = parent
+                child1 = chroms{j}; % child selected to move to next generation
             end
             if (r2 < roulette_wheel(j)) && (found_parent2 == false),
                 found_parent2 = true;
-                child2 = chroms{j}; % child = parent
+                child2 = chroms{j}; % child selected to move to next generation
             end
         end
 
@@ -80,9 +85,11 @@ for x=1:3000,
         end
 
 
-        chroms{i} = child1;
-        chroms{i+(pop/2)} = child2;
+        chroms{i} = child1; % move child to next generation
+        chroms{i+(pop/2)} = child2; % move child to next generation
     end
 end
 
-
+xlabel('time')
+ylabel('cost')
+plot(cost_history)
